1b64e35a027ba7c4a849f98e5af13a32d485ecba
GinaHung <ula.soda@gmail.com>
2016-08-14 00:07:57 +0800

    將selectedHero傳入hero-detail

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { Hero } from './hero';//導出Hero類別
 @Component({
-    moduleId: module.id,
+    // moduleId: module.id,
     selector: 'my-hero-detail',
-    templateUrl: 'hero-detail.component.html'
+    templateUrl: 'hero-detail.component.html',
+    
 })
 export class HeroDetailComponent implements OnInit {
-    
+    //@Input 裝飾器(decorator),宣告hero是從別的元件輸入過來的
+    @Input() 
     hero: Hero;
 }
