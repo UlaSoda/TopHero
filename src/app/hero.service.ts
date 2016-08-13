d4b4935bf703ead8cfe32c7f9113ccb97ce139e6
GinaHung <ula.soda@gmail.com>
2016-08-14 00:17:32 +0800

    改由HeroService藉由getHeroes方法取得英雄數據

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 
+import { HEROES } from './mock-heroes';
 @Injectable()//可被注入
 export class HeroService {//導出供別人使用
   getHeroes() 
   {
-      
+      return HEROES;
   }
 }
