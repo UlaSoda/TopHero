049c1b4ec7a35504383e15021c94933219036a7b
GinaHung <ula.soda@gmail.com>
2016-08-14 00:08:41 +0800

    在AppModule中導入HeroDetailComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';//表單須引用此模組
 import { AppComponent } from './app.component';
 
+import { HeroDetailComponent } from './hero-detail.component';//會使用此模組
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent //會使用此模組
   ],
   imports: [
     BrowserModule,
