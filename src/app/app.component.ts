a5e468ccddbfc83ea8f98f660c8da1ff3226d7e2
GinaHung <ula.soda@gmail.com>
2016-08-14 00:15:08 +0800

    將英雄array獨立到mock-heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,9 @@ import { Hero } from './hero';//導出Hero類別
 export class AppComponent {
   title = 'Tour of Heroes'; //application name
 
-  public heroes = HEROES;
+  // public heroes = HEROES;
+  heroes: Hero[];
+  
   selectedHero: Hero;
 
   onSelect(hero: Hero) 
@@ -23,16 +25,16 @@ export class AppComponent {
 //   id: number;
 //   name: string;
 // }
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+// 移到mock-heroes.ts
+// const HEROES: Hero[] = [
+//   { id: 11, name: 'Mr. Nice' },
+//   { id: 12, name: 'Narco' },
+//   { id: 13, name: 'Bombasto' },
+//   { id: 14, name: 'Celeritas' },
+//   { id: 15, name: 'Magneta' },
+//   { id: 16, name: 'RubberMan' },
+//   { id: 17, name: 'Dynama' },
+//   { id: 18, name: 'Dr IQ' },
+//   { id: 19, name: 'Magma' },
+//   { id: 20, name: 'Tornado' }
+// ];
