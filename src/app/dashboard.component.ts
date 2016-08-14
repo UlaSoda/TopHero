5848e01e030278810750d2289697b95d144b3db2
GinaHung <ula.soda@gmail.com>
2016-08-14 10:44:06 +0800

    儀錶板初始化時顯示前五名英雄

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,7 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-dashboard',
     templateUrl: 'dashboard.component.html'
 })
-export class DashboardComponent{ }
+export class DashboardComponent implements OnInit{
+    heroes: Hero[] = [];
+
+    constructor(private heroService: HeroService){}
+
+    ngOnInit(){//抓取前五名HERO
+        this.heroService.getHeroesSlowly()
+            .then(heroes => this.heroes = heroes.slice(0,5));    
+    }
+
+    gotoDetail(){
+
+    }
+ }
